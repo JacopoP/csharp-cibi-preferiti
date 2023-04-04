09bf3e6c9451bbf76fb39c4124c5b2510698c42b
Jac22 <jacopo.prudentino@gmail.com>
2023-04-04 14:26:13 +0200

    Shown first and last element of the array

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string[] favFood = { "pizza", "lasagne", "spaghetti alla carbonara", "arrosto", "piadina", "Crostata" };
+string[] favFood = { "pizza", "lasagne", "spaghetti alla carbonara", "arrosto", "piadina", "crostata" };
 
 Console.WriteLine($"You have {favFood.Length} favourite foods");
 
 Console.WriteLine("Your favourite foods are");
 for( int i = 0; i < favFood.Length; i++) 
     Console.WriteLine($"{i + 1}. {favFood[i]}" );
+
+Console.WriteLine($"Your favourite food is {favFood[0]}");
+
+Console.WriteLine($"Your not-so-favourite food is {favFood[favFood.Length - 1]}");
