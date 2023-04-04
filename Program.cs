530d671e46d0e8d40025ec53213ed256c487ee71
Jac22 <jacopo.prudentino@gmail.com>
2023-04-04 14:23:00 +0200

    Created food list, shown his length and values

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string[] favFood = { "pizza", "lasagne", "spaghetti alla carbonara", "arrosto", "piadina", "Crostata" };
+
+Console.WriteLine($"You have {favFood.Length} favourite foods");
+
+Console.WriteLine("Your favourite foods are");
+for( int i = 0; i < favFood.Length; i++) 
+    Console.WriteLine($"{i + 1}. {favFood[i]}" );
