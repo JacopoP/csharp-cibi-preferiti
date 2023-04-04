ee9a7a30115bdfe337fd439e6c857b03df9f0606
Jac22 <jacopo.prudentino@gmail.com>
2023-04-04 14:32:19 +0200

    Shown array elements that are in the middle of the array

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string[] favFood = { "pizza", "lasagne", "spaghetti alla carbonara", "arrosto", "piadina", "crostata" };
+string[] favFood = { "pizza", "lasagne", "spaghetti alla carbonara", "arrosto", "piadina", };
 
 Console.WriteLine($"You have {favFood.Length} favourite foods");
 
@@ -12,3 +12,8 @@ for( int i = 0; i < favFood.Length; i++)
 Console.WriteLine($"Your favourite food is {favFood[0]}");
 
 Console.WriteLine($"Your not-so-favourite food is {favFood[favFood.Length - 1]}");
+
+if (favFood.Length % 2 == 0)
+    Console.WriteLine($"Your medium favourite foods are {favFood[favFood.Length / 2]} and {favFood[favFood.Length / 2 - 1]}");
+else
+    Console.WriteLine($"Your medium favourite food is {favFood[favFood.Length / 2]}");
